dbe873b63f63ae05675727f7a83cdaf64595ce6d
Vanok <vanok008@ukr.net>
2024-09-12 11:59:15 +0300

    newsection

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,17 @@
 import styles from "./Contact.module.css";
+import { IoPersonSharp } from "react-icons/io5";
+import { FaPhoneAlt } from "react-icons/fa";
 
 export default function Contact({ contact }) {
   return (
     <>
-      <p className={styles.contactName}>{contact.name}</p>
-      <p className={styles.contactNumber}>{contact.number}</p>
+      <div className={styles.pers}>
+        <IoPersonSharp /> <p className={styles.contactName}>{contact.name}</p>
+        <FaPhoneAlt /> <p className={styles.contactNumber}>{contact.number}</p>
+      </div>
+      <button className={styles.addButton} type="submit">
+        Delete
+      </button>
     </>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -7,7 +7,7 @@ export default function ContactList({ contacts }) {
       <ul className={styles.contactList}>
         {contacts.map((contact) => (
           <li key={contact.id} className={styles.liContact}>
-            <Contact contact={contact} /> {/* Відображаємо один контакт */}
+            <Contact contact={contact} />
           </li>
         ))}
       </ul>
